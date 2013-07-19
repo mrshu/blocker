11d8f7f337e3ff0792cb9aac1e69f38b54cd588b
mr.Shu <mr@shu.io>
2013-07-19 12:26:50 +0200

    added waiter

--- robosim/evaler.js
+++ robosim/evaler.js
@@ -8,25 +8,31 @@ var gamejs = require('gamejs');
 var DummyRobot = require('dummy_robot').DummyRobot;
 
 var handleEvent = function(data) {
-   if (data.todo) {
-      var robot = new DummyRobot();
-      var code = data.todo;
-      
-      //gamejs.worker.post(data.todo);
+    var waiter = function(millis){
+        var time = Date.now();
+        while (Date.now() - time < millis) {
+        }
+    }
 
-      try {
-        eval(code);
-      } catch (e) {
-        // Null is thrown for infinite loop.
-        // Otherwise, abnormal termination is a user error.
-        gamejs.log(e);
-      }
+    if (data.todo) {
+        var robot = new DummyRobot();
+        var code = data.todo;
+
+        //gamejs.worker.post(data.todo);
+
+        try {
+            eval(code);
+        } catch (e) {
+            // Null is thrown for infinite loop.
+            // Otherwise, abnormal termination is a user error.
+            gamejs.log(e);
+        }
 
 
-   } else {
-      gamejs.log('unknown todo');
-   }
+    } else {
+        gamejs.log('unknown todo');
+    }
 }
 gamejs.ready(function() {
-   gamejs.onEvent(handleEvent);
+    gamejs.onEvent(handleEvent);
 })
